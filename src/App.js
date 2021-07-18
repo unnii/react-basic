f9555f5952aade86528f2a00bd6511ea2314b178
unni <unniwaynd@gmail.com>
2021-07-18 23:18:29 +0530

    New react app started

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div>
+        <h1> Unni Krishnan </h1>
+        <h1> Unni Krishnan </h1>
+        <h1> Unni Krishnan </h1>
+        <h1> Unni Krishnan </h1>
+        </div>
+    );
+}
+
+export default App;
